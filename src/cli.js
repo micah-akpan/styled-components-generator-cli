4e86849b14289eb0b1ad1de9c41aa13c697cf27d
Micah Akpan <makpan@gidimobile.com>
2021-06-30 09:46:27 -0400

    Parse CSS and generate styled components structure code

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,15 @@
 import arg from 'arg'
+import path from 'path'
+import fs from 'fs'
+import CSSOM from 'cssom'
+import { generateStyles, generateStyledComponents } from './utils'
 
 function parseArgumentsIntoOptions(rawArgs) {
   const args = arg(
     {
       '--type': String,
       '--quiet': Boolean,
+      '--file': String,
       '-t': '--type',
       '-q': '--quiet',
     },
@@ -15,11 +20,28 @@ function parseArgumentsIntoOptions(rawArgs) {
 
   return {
     type: args['--type'] || 'css',
-    quiet: args['--quiet'] || false
+    quiet: args['--quiet'] || false,
+    file: args['--file']
   }
 }
 
 export function cli(args) {
   let options = parseArgumentsIntoOptions(args)
-  console.log(options);
+  // console.log(options);
+  convertCSS(options.file)
 }
+
+function convertCSS(cssFile) {
+  let filePath = path.resolve(__dirname, cssFile)
+  let stream = fs.createReadStream(filePath, { encoding: 'utf-8' })
+
+  stream.on('data', (data) => {
+    let cssString = CSSOM.parse(data)
+
+    const styles = generateStyles(cssString.cssRules)
+    const components = generateStyledComponents(styles)
+  })
+
+  stream.read()
+}
+
